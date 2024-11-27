381d14c9a862f8860692b084e63b3daf43abcd83
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-11-27 19:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your JS code here. If required.
+
+const circles=document.querySelectorAll('.class');
+const nextbtn=document.getElementById('next');
+const prevbtn=document.getElementById('prev');
+
+let activecircle=1;
+
+UI();
+
+nextbtn.addEventListener('click',()=>{
+    activecircle++;
+if(activecircle>circles.length){
+    activecircle=circles.length;
+}
+UI();
+})
+
+prevbtn.addEventListener('click',()=>{
+    activecircle--;
+    if(activecircle<1){
+        activecircle=1;
+    }
+    UI();
+})
+
+function UI(){
+   circles.forEach((circle,index)=>{
+if(index<activecircle){
+    circle.classList.add('active');
+}
+else{
+ circle.classList.remove('active');    
+}
+   }) 
+
+   prevbtn.disabled=activecircle===1;
+   nextbtn.disabled=activecircle===circles.length; 
+
+}
